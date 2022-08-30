88757080810cdc95b61ccb927798b68aba1ee59b
Zoe21354 <zoetreutens@gmail.com>
2022-08-30 12:13:45 +0200

    music

---
+++ songs.js
@@ -0,0 +1,9 @@
+audioPlayer();
+function audioPlayer(){
+    $("#audioPlayer")[0].src = $("#playlist li a")[0];
+    $
+    $("#playlist li a").click(function(e){e.preventDefault();
+    $("#audioPlayer")[0].src = this;
+    $("#playlist li a").click().play();
+    });
+} 
